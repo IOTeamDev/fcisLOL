f0d4fcbc49b9f6a62715b5f2063da01cd4b10bc4
ibrahim <zeroibrahim49@gmail.com>
2024-02-18 18:33:11 +0200

    Reduced min view height

--- src/app/years/1st/page.tsx
+++ src/app/years/1st/page.tsx
@@ -2,18 +2,18 @@ import LinkCard from "@/src/components/LinkCard";
 import React from "react";
 
 const page = () => {
-  return (
-    <div className="min-w-sreen h-screen flex justify-center items-center   ">
-      <div className="grid grid-cols-2 max-md:grid-cols-1  gap-5 ">
-        <LinkCard href="/years/1st-year/1">
-          <p>first semester </p>
-        </LinkCard>
-        <LinkCard href="/years/1st-year/2">
-          <p>second semester </p>
-        </LinkCard>
-      </div>
-    </div>
-  );
+	return (
+		<div className="min-w-sreen h-screen flex justify-center items-center   ">
+			<div className="grid grid-cols-2 max-md:grid-cols-1  gap-5 ">
+				<LinkCard href="/1">
+					<p>first semester </p>
+				</LinkCard>
+				<LinkCard href="/2">
+					<p>second semester </p>
+				</LinkCard>
+			</div>
+		</div>
+	);
 };
 
 export default page;
