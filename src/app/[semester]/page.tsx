ac36dcbf3e974c22284987e3ddc7b6e9cd242302
M1ghtyMushroom <hilariousbananas@proton.me>
2024-02-18 19:51:55 +0200

    Added admin approval page

--- src/app/[semester]/page.tsx
+++ src/app/[semester]/page.tsx
@@ -3,33 +3,33 @@ import { getSemesterSubjects } from "@/src/lib/db/subjects/getSemesterSubjects";
 import React from "react";
 
 const page = async ({ params }: { params: { semester: string } }) => {
-	let currentSemesterSubject: any[] = [];
-	// check if current semester is in the database and  has subjects
-	try {
-		currentSemesterSubject = await getSemesterSubjects(Number(params.semester));
-		if (currentSemesterSubject.length == 0) throw new Error();
-	} catch (err) {
-		return (
-			<>
-				<div className="w-full min-h-[calc(100vh-75px)] flex justify-center items-center">
-					<p className="text-4xl">404 POP LOL </p>
-				</div>
-			</>
-		);
-	}
-	return (
-		<div className="min-w-sreen min-h-screen flex justify-center items-center  p-3 ">
-			<div className="grid grid-cols-4 max-xl:grid-cols-2 max-md:grid-cols-1  gap-5 ">
-				{currentSemesterSubject.map((subject, index) => (
-					<>
-						<LinkCard key={index} href={`${params.semester}/${subject.id}`}>
-							<p>{subject.name}</p>
-						</LinkCard>
-					</>
-				))}
-			</div>
-		</div>
-	);
+  let currentSemesterSubject: any[] = [];
+  // check if current semester is in the database and  has subjects
+  try {
+    currentSemesterSubject = await getSemesterSubjects(Number(params.semester));
+    if (currentSemesterSubject.length == 0) throw new Error();
+  } catch (err) {
+    return (
+      <>
+        <div className="w-full min-h-[calc(100vh-75px)] flex justify-center items-center">
+          <p className="text-4xl font-extrabold">404 POP LOL </p>
+        </div>
+      </>
+    );
+  }
+  return (
+    <div className="min-w-sreen min-h-screen flex justify-center items-center  p-3 ">
+      <div className="grid grid-cols-4 max-xl:grid-cols-2 max-md:grid-cols-1  gap-5 ">
+        {currentSemesterSubject.map((subject, index) => (
+          <>
+            <LinkCard key={index} href={`${params.semester}/${subject.id}`}>
+              <p>{subject.name}</p>
+            </LinkCard>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default page;
---
+++ src/app/admin/approve/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>admin approve</div>;
+}
+
+export default page;
