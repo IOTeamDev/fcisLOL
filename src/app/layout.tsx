ffbe708e784b81706bf52d6c2a18023b9a04b2bc
M1ghtyMushroom <hilariousbananas@proton.me>
2024-02-18 23:43:14 +0200

    metedata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import { Toaster } from "react-hot-toast";
 import NavBar from "../components/NavBar";
 import ThemeProviderWrapper from "../lib/ThemeProviderWrapper";
 import { GeistSans } from "geist/font/sans";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "LOL",
+};
 
 export default function RootLayout({
   children,
@@ -14,7 +19,7 @@ export default function RootLayout({
       <body>
         <Toaster position="top-right" reverseOrder />
         <ThemeProviderWrapper>
-					<NavBar />
+          <NavBar />
           <main>{children}</main>
         </ThemeProviderWrapper>
       </body>
