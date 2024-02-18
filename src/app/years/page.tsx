50a2fcf1d21250a16bede28d6531d0ea6951f523
M1ghtyMushroom <hilariousbananas@proton.me>
2024-02-18 16:17:30 +0200

    Modifing the main button

--- src/app/years/page.tsx
+++ src/app/years/page.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import Card from "@/src/lib/components/Card";
-import LinkCard from "@/src/lib/components/LinkCard";
+import Card from "@/src/components/Card";
+import LinkCard from "@/src/components/LinkCard";
 
 const page = () => {
-	return (
-		<div className="min-w-sreen h-screen flex justify-center items-center   ">
-			<div className="grid grid-cols-4 max-xl:grid-cols-2 max-md:grid-cols-1  gap-5 ">
-				<Card> </Card>
-				<Card> </Card>
-				<Card> </Card>
-				<LinkCard href="/years/first-year">
-					<p>1st grade </p>
-				</LinkCard>
-			</div>
-		</div>
-	);
+  return (
+    <div className="min-w-sreen h-screen flex justify-center items-center   ">
+      <div className="grid grid-cols-4 max-xl:grid-cols-2 max-md:grid-cols-1  gap-5 ">
+        <Card> </Card>
+        <Card> </Card>
+        <Card> </Card>
+        <LinkCard href="/years/first-year">
+          <p>1st grade </p>
+        </LinkCard>
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,21 +1,20 @@
 import React from "react";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 
-const photos = [
-  "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2t0NDFnYzV0d3h4OHl5MHc0aG90eXRyNHd6YmxydDd3cXlibnpoMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fhAwk4DnqNgw8/giphy.gif",
-  "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaXdzZmQycTB6bnJ3dDNnM2ZyOHhsYzI5N3I3cGFkZTNlM3NzN3ludCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/12vJgj7zMN3jPy/giphy.gif",
-  "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMHJrdTIzMzUyYTEyNzJ2bDBtM2luYjdmanh6OW42MnA5cmdwYzd0dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/pFwRzOLfuGHok/giphy.gif",
-  // Add more photo URLs here
-];
+// use this to randomize the image
+// const photos = [
+//   "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2t0NDFnYzV0d3h4OHl5MHc0aG90eXRyNHd6YmxydDd3cXlibnpoMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fhAwk4DnqNgw8/giphy.gif",
+//   "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaXdzZmQycTB6bnJ3dDNnM2ZyOHhsYzI5N3I3cGFkZTNlM3NzN3ludCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/12vJgj7zMN3jPy/giphy.gif",
+//   "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMHJrdTIzMzUyYTEyNzJ2bDBtM2luYjdmanh6OW42MnA5cmdwYzd0dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/pFwRzOLfuGHok/giphy.gif",
+//   // Add more photo URLs here
+// ];
 
-function getRandomPhoto() {
-  const randomIndex = Math.floor(Math.random() * photos.length);
-  return photos[randomIndex];
-}
+// function getRandomPhoto() {
+//   const randomIndex = Math.floor(Math.random() * photos.length);
+//   return photos[randomIndex];
+// }
 
 export default function Hero() {
-  const randomPhoto = getRandomPhoto();
+  // const randomPhoto = getRandomPhoto();
 
   return (
     <section className="w-full py-12 md:py-24 lg:py-32">
@@ -25,7 +24,7 @@ export default function Hero() {
             alt="Image"
             className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full"
             height="310"
-            src={randomPhoto}
+            src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2t0NDFnYzV0d3h4OHl5MHc0aG90eXRyNHd6YmxydDd3cXlibnpoMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fhAwk4DnqNgw8/giphy.gif"
             width="550"
           />
           <div className="flex flex-col justify-center space-y-4">
@@ -38,10 +37,12 @@ export default function Hero() {
               </p>
             </div>
             <div className="flex flex-col gap-2 min-[400px]:flex-row">
-              <Button type="submit">Start Studying!</Button>
-              <Link href={"/register"}>
-                <Button>Be a Creator</Button>
-              </Link>
+              <a
+                href="/years"
+                className="w-full flex justify-center border-[#020817] border-2 py-2 rounded-xl bg-[#020817] text-white font-bold dark:bg-white dark:text-[#020817]"
+              >
+                Start Now!
+              </a>
             </div>
           </div>
         </div>
