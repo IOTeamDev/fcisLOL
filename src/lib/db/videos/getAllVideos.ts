709e07eb14da7e487edaede543857f80ff622ce3
M1ghtyMushroom <hilariousbananas@proton.me>
2024-02-19 21:24:34 +0200

    FEAT: Made getAllVideos.ts: get all, panding, approved or rejected videos

---
+++ src/lib/db/videos/getAllVideos.ts
@@ -0,0 +1,49 @@
+"use server";
+import prisma from "@/src/lib/PrismaClient";
+
+enum Status {
+  PENDING = "PENDING",
+  APPROVED = "APPROVED",
+  REJECTED = "REJECTED",
+}
+
+export const getAllVideos = async (status?: Status) => {
+  switch (status) {
+    case Status.PENDING:
+      try {
+        const videos = await prisma.video.findMany({
+          where: { status: "PENDING" },
+        });
+        return videos;
+      } catch (err) {
+        throw err;
+      }
+    case Status.APPROVED:
+      try {
+        const videos = await prisma.video.findMany({
+          where: { status: "APPROVED" },
+        });
+        return videos;
+      } catch (err) {
+        throw err;
+      }
+    case Status.REJECTED:
+      try {
+        const videos = await prisma.video.findMany({
+          where: { status: "REJECTED" },
+        });
+        return videos;
+      } catch (err) {
+        throw err;
+      }
+    default:
+      try {
+        const videos = await prisma.video.findMany();
+        return videos;
+      } catch (err) {
+        throw err;
+      }
+  }
+};
+
+prisma.$disconnect();
