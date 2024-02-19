eeb0ee2bbd236cf936141dee656ea512b75686f1
M1ghtyMushroom <hilariousbananas@proton.me>
2024-02-20 00:38:48 +0200

    FIX: admin layout Protection

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -3,13 +3,14 @@ import { getServerSession } from "next-auth";
 import { getUserByEmail } from "@/src/lib/db/user/getUser";
 import NotFound from "@/src/components/NotFound";
 
-async function layout() {
+async function layout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession();
   const user = await getUserByEmail(session?.user?.email);
 
   if (user?.role === "USER") {
     return <NotFound />;
   }
+  return <div>{children}</div>;
 }
 
 export default layout;
