c8c036bd3b894a823cf5ac2c1711a87cfd5787a7
ibrahim <zeroibrahim49@gmail.com>
2024-02-19 17:04:55 +0200

    Removed map-key warning

--- src/app/levels/[level]/page.tsx
+++ src/app/levels/[level]/page.tsx
@@ -2,22 +2,23 @@ import LinkCard from "@/src/components/LinkCard";
 import { getSemestersOfLevel } from "@/src/lib/db/videos/getSemestersOfLevel";
 
 const page = async ({ params }: { params: { level: string } }) => {
-  const levelSemesters = await getSemestersOfLevel(parseInt(params.level[0]));
+	const levelSemesters = await getSemestersOfLevel(parseInt(params.level[0]));
 
-  return (
-    <div className="min-w-sreen flex-grow flex justify-center items-center   ">
-      <div className="grid grid-cols-1 gap-4">
-        {levelSemesters.map((semester) => (
-          <LinkCard
-            href={`/semesters/${semester.id}`}
-            className="w-[500px] max-[550px]:w-[400px] max-[500px]:w-[300px]"
-          >
-            <p>{semester.name}</p>
-          </LinkCard>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="min-w-sreen flex-grow flex justify-center items-center   ">
+			<div className="grid grid-cols-1 gap-4">
+				{levelSemesters.map((semester, index) => (
+					<LinkCard
+						key={index}
+						href={`/semesters/${semester.id}`}
+						className="w-[500px] max-[550px]:w-[400px] max-[500px]:w-[300px]"
+					>
+						<p>{semester.name}</p>
+					</LinkCard>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default page;
