d6bb737deee5c8296e3d21e259ec8709ba90b417
M1ghtyMushroom <hilariousbananas@proton.me>
2024-02-20 21:39:32 +0200

    FEAT: Videos are now showed from newer to older

--- src/lib/db/videos/updateVideo.ts
+++ src/lib/db/videos/updateVideo.ts
@@ -2,14 +2,15 @@
 import prisma from "@/src/lib/PrismaClient";
 
 export const updateVideo = async (videoId: number, data: any) => {
-	const video = await prisma.video.update({
-		where: {
-			id: videoId,
-		},
-		data: { ...data },
-		// if a field is not provided, it will not be updated
-	});
-	return video;
+  const video = await prisma.video.update({
+    where: {
+      id: videoId,
+    },
+    data: {
+      ...data,
+      updatedAt: new Date(),
+    },
+    // if a field is not provided, it will not be updated
+  });
+  return video;
 };
-
-prisma.$disconnect();
