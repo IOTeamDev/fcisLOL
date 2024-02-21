dd2d4e61d4f09c758c66f028017305fb7be474e2
M1ghtyMushroom <hilariousbananas@proton.me>
2024-02-21 19:32:46 +0200

    fixed padding and margin issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,26 +7,26 @@ import type { Metadata } from "next";
 import Footer from "../components/Footer";
 
 export const metadata: Metadata = {
-	title: "LOL",
+  title: "LOL",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en" className={GeistSans.className}>
-			<body className="min-h-screen flex flex-col">
-				<Toaster position="top-center" reverseOrder />
-				<ThemeProviderWrapper>
-					<NavBar />
-					<main className="flex-grow flex flex-col min-h-[calc(100vh-30px)]">
-						{children}
-					</main>
-					<Footer />
-				</ThemeProviderWrapper>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={GeistSans.className}>
+      <body className="min-h-screen flex flex-col">
+        <Toaster position="top-center" reverseOrder />
+        <ThemeProviderWrapper>
+          <NavBar />
+          <main className="flex-grow flex flex-col min-h-[calc(100vh-30px)]">
+            {children}
+          </main>
+          <Footer />
+        </ThemeProviderWrapper>
+      </body>
+    </html>
+  );
 }
--- src/app/subjects/[subjectId]/pages/VideoPage.tsx
+++ src/app/subjects/[subjectId]/pages/VideoPage.tsx
@@ -2,41 +2,40 @@ import React from "react";
 import AddVideoButton from "../AddVideoButton";
 import VideoComponent from "@/src/components/VideoComponent";
 interface Props {
-	user: any;
-	currentSubject: any;
+  user: any;
+  currentSubject: any;
 }
 
 const videoPage = ({ currentSubject, user }: Props) => {
-	if (currentSubject?.videos.length == 0) {
-		return (
-			<div className="w-full flex-grow flex flex-col justify-center items-center">
-				<h2 className="text-5xl font-bold">{currentSubject?.name}</h2>
-
-				<div className="flex flex-col items-center">
-					<p className="black:text-white text-center text-4xl font-extrabold p-4 max-md:text-xl opacity-70">
-						No Videos Here Yet 😓 <br /> Why Don't You Add One?! 😏
-					</p>
-					<AddVideoButton subjectId={currentSubject.id} user={user} />
-				</div>
-			</div>
-		);
-	}
-	return (
-		<div className="flex-grow flex mt-10 flex-col gap-9 items-center ">
-			<h2 className="text-5xl font-bold">{currentSubject?.name}</h2>
-			<AddVideoButton subjectId={currentSubject.id} user={user} />
-			<div className="grid p-10 grid-cols-4 max-[1700px]:grid-cols-3 max-[1100px]:grid-cols-2 max-[780px]:grid-cols-1 gap-4">
-				{currentSubject?.videos.map((video: any, index: any) => {
-					video.subject = currentSubject;
-					return (
-						<div key={index}>
-							<VideoComponent video={video} target={`/video/${video.id}`} />
-						</div>
-					);
-				})}
-			</div>
-		</div>
-	);
+  if (currentSubject?.videos.length == 0) {
+    return (
+      <div className="w-full h-screen flex-grow flex flex-col justify-center items-center">
+        <h2 className="text-5xl font-bold -mt-40">{currentSubject?.name}</h2>
+        <div className="flex flex-col items-center">
+          <p className="black:text-white text-center text-4xl font-extrabold p-4 max-md:text-xl opacity-70">
+            No Videos Here Yet 😓 <br /> Why Don't You Add One?! 😏
+          </p>
+          <AddVideoButton subjectId={currentSubject.id} user={user} />
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="flex-grow flex mt-10 flex-col gap-9 items-center ">
+      <h2 className="text-5xl font-bold">{currentSubject?.name}</h2>
+      <AddVideoButton subjectId={currentSubject.id} user={user} />
+      <div className="grid p-10 grid-cols-4 max-[1700px]:grid-cols-3 max-[1100px]:grid-cols-2 max-[780px]:grid-cols-1 gap-4">
+        {currentSubject?.videos.map((video: any, index: any) => {
+          video.subject = currentSubject;
+          return (
+            <div key={index}>
+              <VideoComponent video={video} target={`/video/${video.id}`} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default videoPage;
