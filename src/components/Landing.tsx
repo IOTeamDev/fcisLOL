80708e62106f997cbd2fbd7e6f3160070cac2596
M1ghtyMushroom <hilariousbananas@proton.me>
2024-02-22 08:29:35 +0200

    REFINE: Landing Page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const Landing = () => {
   return (
@@ -13,21 +14,24 @@ const Landing = () => {
               Videos 🎬, Notes 📝, Recordings 📹, Exams 🖊... and more
             </p>
           </div>
-          <img
-            src="
-          https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYXB5Y3VyYWZtNGs4MzI4ZXIzeWp2YjV6YTc1bmR0eGZuaTBxdG03bSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/JlpjgShzDsrMIyFu5U/giphy.gif"
+          <Image
+            src="/searchblack.svg"
             alt="We Got You!"
-            className="w-[700px] h-[600px] aspect-square overflow-hidden rounded-xl object-cover object-center max-[900px]:-mt-10"
+            className="w-[600px] h-[500px] aspect-square rounded-xl object-cover object-center"
+            width={700}
+            height={600}
           />
         </section>
       </div>
       {/**=============================**/}
       <div className="w-full h-screen flex flex-col justify-center items-center ">
         <section className="p-10 w-full h-4/5 flex justify-center items-center max-[900px]:flex-col">
-          <img
-            src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2h2YnZoc2FhcDY4dTlxaGdhNTdvZ2w0aWNyM244MW5ydzB3ZnM5YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3ohs83OwfAqZMnwfgQ/giphy.gif"
+          <Image
+            src="/organization.svg"
             alt="We Got You!"
-            className=" w-[700px] max-[900px]:w-[1400px] h-[600px] aspect-square overflow-hidden rounded-xl object-cover object-center max-[900px]:-mt-10"
+            className="w-[600px] h-[500px] aspect-square overflow-hidden rounded-xl object-cover object-center max-[900px]:-mt-10"
+            width={700}
+            height={600}
           />
           <div className="sm:ml-10 sm:w-1/">
             <h1 className="sm:text-7xl text-5xl font-bold py-2 max-[900px]:py-4 max-[900px]:text-center max-[500px]:text-4xl">
@@ -52,10 +56,12 @@ const Landing = () => {
               📤
             </p>
           </div>
-          <img
-            src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHIwZm5ud3hwYmRodTA1bHI4em42ZGZvaTMyZDJ0dGJ0dG44bjk4cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VJY3zeoK87CLBKnqqm/giphy.gif"
-            alt="Add Your Own!"
-            className="w-[700px] h-[600px] aspect-square overflow-hidden rounded-xl object-cover object-center max-[900px]:-mt-10"
+          <Image
+            src="/uploud.svg"
+            alt="We Got You!"
+            className="w-[600px] h-[500px] aspect-square overflow-hidden rounded-xl object-cover object-center max-[900px]:-mt-10"
+            width={600}
+            height={500}
           />
         </section>
       </div>
