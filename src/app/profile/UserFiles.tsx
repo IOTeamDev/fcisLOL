db7d091a3e6be6d1eea7d47e0f3dca026d09b4c2
ibrahim <zeroibrahim49@gmail.com>
2024-02-24 20:17:09 +0200

    Only approved files show in profile

--- src/app/profile/UserFiles.tsx
+++ src/app/profile/UserFiles.tsx
@@ -2,11 +2,14 @@ import FileComponent from "@/src/components/FileComponent";
 import React from "react";
 
 const UserFiles = ({ userFiles }: { userFiles: any }) => {
+	const approvedFiles = userFiles.filter(
+		(file: any) => file.status === "APPROVED"
+	);
 	return (
 		<div className="space-y-2 py-3">
 			<h2 className="text-2xl  font-bold">Recent Files</h2>
 			<div className="grid py-4 grid-cols-4 max-[1100px]:grid-cols-3 max-[780px]:grid-cols-2 max-[410px]:grid-cols-1 max- gap-4">
-				{userFiles.map((file: any, index: any) => {
+				{approvedFiles.map((file: any, index: any) => {
 					return (
 						<div key={index}>
 							<FileComponent file={file} target={`/files/${file.id}`} />
