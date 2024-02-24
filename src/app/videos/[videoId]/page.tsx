bbb1bde3a280f967e320ee4029dfa35fb5273d74
M1ghtyMushroom <hilariousbananas@proton.me>
2024-02-24 16:22:38 +0200

    FIX: 2 Bugs fixed: pending vids on profile are the only showed ones, pending vids are not accessable any more from the url by id

--- src/app/videos/[videoId]/page.tsx
+++ src/app/videos/[videoId]/page.tsx
@@ -8,41 +8,41 @@ import AvatarIcon from "@/src/components/ui/avatarIcon";
 import NotFound from "@/src/components/NotFound";
 
 const page = async ({ params }: { params: { videoId: string } }) => {
-	const video = await getVideoById(Number(params.videoId));
+  const video = await getVideoById(Number(params.videoId));
 
-	if (video) {
-		const user = await getUserById(video?.userId);
-		return (
-			<div className="w-full flex flex-col justify-center items-center p-8">
-				<h1 className="text-4xl max-[1000px]:text-2xl max-[500px]:text-xl font-bold pb-4 text-center">
-					{video?.title}
-				</h1>
-				<div className="w-full flex max-[700px]:flex-col justify-around items-center pb-12">
-					{video.description ? (
-						<p className="text-lg max-[1000px]:text-sm max-[500px]:text-xs opacity-65 pb-8">
-							{video.description}
-						</p>
-					) : null}
-					<div className="flex items-center">
-						<p>Added by:&nbsp;</p>
-						<AvatarIcon width="32" height="32" />
-						&nbsp;
-						<Link
-							href={`/profile/${video.userId}`}
-							className="text-lg max-[1000px]:text-sm max-[500px]:text-xs underline"
-						>
-							{user?.firstName} {user?.lastName}
-						</Link>
-					</div>
-				</div>
-				<Card className="">
-					<EmbedYTVideo url={video.url} />
-				</Card>
-			</div>
-		);
-	} else {
-		return <NotFound />;
-	}
+  if (video && video?.status === "APPROVED") {
+    const user = await getUserById(video?.userId);
+    return (
+      <div className="w-full flex flex-col justify-center items-center p-8">
+        <h1 className="text-4xl max-[1000px]:text-2xl max-[500px]:text-xl font-bold pb-4 text-center">
+          {video?.title}
+        </h1>
+        <div className="w-full flex max-[700px]:flex-col justify-around items-center pb-12">
+          {video.description ? (
+            <p className="text-lg max-[1000px]:text-sm max-[500px]:text-xs opacity-65 pb-8">
+              {video.description}
+            </p>
+          ) : null}
+          <div className="flex items-center">
+            <p>Added by:&nbsp;</p>
+            <AvatarIcon width="32" height="32" />
+            &nbsp;
+            <Link
+              href={`/profile/${video.userId}`}
+              className="text-lg max-[1000px]:text-sm max-[500px]:text-xs underline"
+            >
+              {user?.firstName} {user?.lastName}
+            </Link>
+          </div>
+        </div>
+        <Card className="">
+          <EmbedYTVideo url={video.url} />
+        </Card>
+      </div>
+    );
+  } else {
+    return <NotFound />;
+  }
 };
 
 export default page;
--- src/lib/db/user/getUserVideos.ts
+++ src/lib/db/user/getUserVideos.ts
@@ -1,18 +1,22 @@
 "use server";
 import prisma from "@/src/lib/PrismaClient";
 
-export const getUserVideos = async (user: number) => {
-	try {
-		const videos = await prisma.video.findMany({
-			where: {
-				userId: user,
-			},
-		});
+export const getUserVideos = async (
+  userId: number,
+  status: "PENDING" | "APPROVED" | "REJECTED"
+) => {
+  try {
+    const videos = await prisma.video.findMany({
+      where: {
+        userId: userId,
+        status: status,
+      },
+    });
 
-		return videos;
-	} catch (err) {
-		throw err;
-	}
+    return videos;
+  } catch (err) {
+    throw err;
+  }
 };
 
 prisma.$disconnect();
