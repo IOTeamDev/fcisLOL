3e0e74df5d06e78203a1277fef1e96332639c125
ibrahim <zeroibrahim49@gmail.com>
2024-02-24 18:50:11 +0200

    Fixed 404 gif Image:{height}

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -4,9 +4,10 @@ const NotFound = () => {
 	return (
 		<div className="w-full flex-grow flex flex-col justify-center items-center">
 			<Image
+				width={700}
+				height={200}
 				src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmdsaHUxYnlqc3R4NHMxNnEzbTNkM242OGlhNmNwODhtamgyeTl3cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gKsJUddjnpPG0/giphy.gif"
 				alt="_"
-				width={700}
 			/>
 			<p className="text-4xl font-extrabold absolute text-white">404 POP LOL</p>
 		</div>
