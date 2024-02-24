3026224fd8da439bb803049a4927d2dfbc79238c
ibrahim <zeroibrahim49@gmail.com>
2024-02-24 17:28:38 +0200

    Fixed Image error

--- next.config.js
+++ next.config.js
@@ -4,20 +4,19 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                // https://i.ytimg.com/vi/XDxr8W-9Yz4/w
-                //https://drive.google.com/
                 protocol: 'https',
                 hostname: 'i.ytimg.com',
                 port: '',
                 pathname: '/**',
             }, {
                 protocol: 'https',
-                hostname: 'images.unsplash.com',
+                hostname: '***',
                 port: '',
                 pathname: '/**'
             }
         ],
     },
+
 }
 
 module.exports = nextConfig
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -23,41 +23,39 @@ const page = async () => {
 	const userVideos = user?.videos;
 	return (
 		<div className="flex-grow flex justify-center items-center relative">
-			<>
-				<Card className="max-w-[70%] max-md:max-w-[100%] border-none my-20">
-					<CardContent className="space-y-4">
-						<div className="flex space-x-4 pb-10 border-b">
-							<div className="flex items-center space-x-4 ">
-								<AvatarIcon width="96" height="96" />
+			<Card className="max-w-[70%] max-md:max-w-[100%] border-none my-20">
+				<CardContent className="space-y-4">
+					<div className="flex space-x-4 pb-10 border-b">
+						<div className="flex items-center space-x-4 ">
+							<AvatarIcon width="96" height="96" />
 
-								<div className="space-y-1.5 max-sm:flex max-sm:flex-col max-sm:items-center">
-									<h1 className="text-3xl font-bold max-lg:text-xl">
-										{user?.firstName} {user?.lastName}
-									</h1>
-									<div className="flex">
-										<EditProfileButton user={user} />
-										<LogoutButton />
-									</div>
+							<div className="space-y-1.5 max-sm:flex max-sm:flex-col max-sm:items-center">
+								<h1 className="text-3xl font-bold max-lg:text-xl">
+									{user?.firstName} {user?.lastName}
+								</h1>
+								<div className="flex">
+									<EditProfileButton user={user} />
+									<LogoutButton />
 								</div>
 							</div>
 						</div>
-						<Tabs defaultValue="videos">
-							<div className="flex items-center justify-center w-full my-2">
-								<TabsList>
-									<TabsTrigger value="videos">Videos</TabsTrigger>
-									<TabsTrigger value="files">Files</TabsTrigger>
-								</TabsList>
-							</div>
-							<TabsContent value="videos">
-								<UserVideos userVideos={userVideos} />
-							</TabsContent>
-							<TabsContent value="files">
-								<UserFiles userFiles={user?.files} />
-							</TabsContent>
-						</Tabs>
-					</CardContent>
-				</Card>
-			</>
+					</div>
+				</CardContent>
+			</Card>
+			<Tabs defaultValue="videos">
+				<div className="flex items-center justify-center w-full my-2">
+					<TabsList>
+						<TabsTrigger value="videos">Videos</TabsTrigger>
+						<TabsTrigger value="files">Files</TabsTrigger>
+					</TabsList>
+				</div>
+				<TabsContent value="videos">
+					<UserVideos userVideos={userVideos} />
+				</TabsContent>
+				<TabsContent value="files">
+					<UserFiles userFiles={user?.files} />
+				</TabsContent>
+			</Tabs>
 		</div>
 	);
 };
