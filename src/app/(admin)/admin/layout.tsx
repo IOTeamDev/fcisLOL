8a37de82e3d75c4002be43682f90868a1cfbed84
ibrahim <zeroibrahim49@gmail.com>
2024-02-24 11:45:56 +0200

    Only superAdmin can accept videos

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -4,14 +4,14 @@ import { getUserByEmail } from "@/src/lib/db/user/getUser";
 import NotFound from "@/src/components/NotFound";
 
 async function layout({ children }: { children: React.ReactNode }) {
-  const session = await getServerSession();
-  const user = await getUserByEmail(session?.user?.email);
+	const session = await getServerSession();
+	const user = await getUserByEmail(session?.user?.email);
 
-  console.log(user)
-  if (user?.role === "USER" || !user) {
-    return <NotFound />;
-  }
-  return <div>{children}</div>;
+	console.log(user);
+	if (user?.role !== "SUPERADMIN" || !user) {
+		return <NotFound />;
+	}
+	return <div>{children}</div>;
 }
 
 export default layout;
